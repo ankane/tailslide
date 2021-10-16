ace732925d6f101a7f11844084447efa8104f9d2
Andrew Kane <andrew@ankane.org>
2021-10-15 20:03:26 -0700

    Added long description [skip ci]

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup
 
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
 setup(
     name='tailslide',
     version='0.1.1',
     description='Median and percentile for Django models and MongoEngine documents',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/ankane/tailslide',
     author='Andrew Kane',
     author_email='andrew@ankane.org',

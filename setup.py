1f36b19136af3e28a1fc8e706b5f0f4025107b16
Andrew Kane <andrew@ankane.org>
2024-06-30 17:34:38 -0700

    Version bump to 0.2.0 [skip ci]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setup(
     name='tailslide',
-    version='0.1.1',
+    version='0.2.0',
     description='Median and percentile for Django and MongoEngine',
     long_description=long_description,
     long_description_content_type='text/markdown',
